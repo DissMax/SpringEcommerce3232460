2941d4a5503355ee06d9b678c97645c40e410e98
dilanlopez1518@gmail.com <dilanlopez1518@gmail.com>
2025-11-05 17:41:18 -0500

    v0.1 clasesPaqueteModelo_diadloli_05112025

---
+++ src/main/java/com/example/demo/model/Orden.java
@@ -0,0 +1,98 @@
+package com.example.demo.model;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ordenes")
+public class Orden {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //anotacion JPA
+	private Integer id;
+	private String numero;
+	private Date fechaCreacion;
+	private Double total;
+	
+	@ManyToOne
+	private Usuario Usuario;
+	
+	@OneToMany(mappedBy = "orden")
+	private List<DetalleOrden> detalle;
+
+	public Orden() {
+		super();
+	}
+
+	public Orden(Integer id, String numero, Date fechaCreacion, Double total) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.fechaCreacion = fechaCreacion;
+		this.total = total;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	public Usuario getUsuario() {
+		return Usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		Usuario = usuario;
+	}
+
+	public List<DetalleOrden> getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(List<DetalleOrden> detalle) {
+		this.detalle = detalle;
+	}
+
+	@Override
+	public String toString() {
+		return "Orden [id=" + id + ", numero=" + numero + ", fechaCreacion=" + fechaCreacion + ", total=" + total + "]";
+	}
+	
+	
+
+}
